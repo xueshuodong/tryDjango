788131637d955c78dcd63cfcb6908ff6703ecb88
xdong <xueshuo.dong@fonetic.com>
2022-04-14 09:25:39 +0200

    try md help

---
+++ help.md
@@ -0,0 +1,3 @@
+* product_create.html
+    <form action='/search/' method='POST'>
+    ![image info](./images/action_search_ex.png)
--- src/products/views.py
+++ src/products/views.py
@@ -4,16 +4,20 @@ from .forms import ProductForm
 from .models import Product
 # Create your views here.
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm() #rerender
-    context = {
-        'form': form
-    }
-    return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm() #rerender
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
+def product_create_view(request):
+    context = {}
+    return render(request, "products/product_create.html", context)
+    
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     # context = {
