20c56bba46416aeb4163881f494180d407ce7092
xdong <xueshuo.dong@fonetic.com>
2022-04-15 08:18:01 +0200

    ex validation for forms

--- src/products/forms.py
+++ src/products/forms.py
@@ -5,18 +5,8 @@ from .models import Product
 #different types of using forms...
 
 class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = {
-            'title',
-            'description',
-            'price'
-        }
-
-class RawProductForm(forms.Form):
-    title       = forms.CharField(label='', widget=forms.TextInput(attrs={
-                                                        'placeholder': 'your title'
-                                                        }))#default required=True, see in the doc
+    title       = forms.CharField(label='', widget=forms.TextInput(attrs={ 'placeholder': 'your title'}))#default required=True, see in the doc
+    email       = forms.EmailField()
     description = forms.CharField(required=False, widget=forms.Textarea(
                                                         attrs={
                                                             'placeholder': 'your description',
@@ -24,6 +14,38 @@ class RawProductForm(forms.Form):
                                                             'id': 'my-id-for-textarea',
                                                             'rows': 3,
                                                             'cols': 20,
-                                                        }
-                                                    ))
+                                                        } ))
+    price       = forms.DecimalField(initial=199.99)    
+    class Meta:
+        model = Product
+        fields = {
+            'title',
+            'description',
+            'price'
+        }
+    
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if not 'CFE' in title:
+            raise forms.ValidationError("This is not a valid title")
+        else:
+            return title
+    
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not a valid email")
+        else:
+            return email
+
+class RawProductForm(forms.Form):
+    title       = forms.CharField(label='', widget=forms.TextInput(attrs={ 'placeholder': 'your title'}))#default required=True, see in the doc
+    description = forms.CharField(required=False, widget=forms.Textarea(
+                                                        attrs={
+                                                            'placeholder': 'your description',
+                                                            'class': 'new-class-name two',
+                                                            'id': 'my-id-for-textarea',
+                                                            'rows': 3,
+                                                            'cols': 20,
+                                                        }))
     price       = forms.DecimalField(initial=199.99)
--- src/products/views.py
+++ src/products/views.py
@@ -4,29 +4,29 @@ from .forms import ProductForm, RawProductForm
 from .models import Product
 # Create your views here.
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm() #rerender
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", context)
-
 def product_create_view(request):
-    # print('get')
-    # print(request.GET)
-    # print('request title '+request.GET['title'])
-    # print('post')
-    # print(request.POST)
-    if request.method == 'POST':
-        my_new_title = request.POST.get('title')
-        # print('new title ')
-        print(my_new_title)
-        #Product.objects.create(title=my_new_title)
-    context = {}
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm() #rerender
+    context = {
+        'form': form
+    }
     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     # print('get')
+#     # print(request.GET)
+#     # print('request title '+request.GET['title'])
+#     # print('post')
+#     # print(request.POST)
+#     if request.method == 'POST':
+#         my_new_title = request.POST.get('title')
+#         # print('new title ')
+#         print(my_new_title)
+#         #Product.objects.create(title=my_new_title)
+#     context = {}
+#     return render(request, "products/product_create.html", context)
 
 def product_form_view(request):
     my_form = RawProductForm() #equal to RawProductForm(request.GET)
